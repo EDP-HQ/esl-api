155a2f175c58cf0c41874c4d4fedd198002a4432
Sheik Hazrin Sheik Othman <sheikhazrin@kiswire.com>
2023-11-30 08:29:35 +0800

    simplify index

--- index.js
+++ index.js
@@ -30,12 +30,39 @@ var server = app.listen(process.env.PORT || sPort, function () {
   console.log("App now running on port", port);
 });
 
+// Define a function to handle the common logic
+function handleMachineRunningStatus(req, res, reportName) {
+  // Collection of parameter.
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  });
+
+  const code = req.query.code;
+  console.log(formattedDate + " : " + code);
+
+  const parameters = [{ name: "v_MCCode", value: code }];
+
+  database.executeStoredProcedure(
+    res,
+    kcvldbConfig,
+    reportName,
+    parameters
+  );
+}
+
 // ########################################
 // ###### API
 // ########################################
 
 app.get("/", wIntro);
-app.get("/mrsd13", getMRSd13);
 
 // ########################################
 // ###### FUNCTION
@@ -43,70 +70,71 @@ app.get("/mrsd13", getMRSd13);
 function wIntro(req, res) {
   res.send("🚀 Kiswire KCVL API now running on port " + sPort);
 }
-function getMRSd13(req, res) {
-  res.send("🚀 getMRSd13" + sPort);
-}
-// function getMRSd14(req, res) {
-//   console.log('MRSd14')
-//   // const group = req.query.group;
-//   const MCCode = req.query.MCCode;
 
-//   const parameters = [{ name: "v_MCCode", value: MCCode }];
-//   console.log (parameters)
+// Route for /d14
+app.get("/d14", function (req, res) {
+  handleMachineRunningStatus(req, res, "RPT_D14_Machine_Running_Status");
+});
+
+// Route for /d15
+app.get("/d15", function (req, res) {
+  handleMachineRunningStatus(req, res, "RPT_D15_Machine_Running_Status");
+});
+
+// Route for /d16
+app.get("/d16", function (req, res) {
+  handleMachineRunningStatus(req, res, "RPT_D16_Machine_Running_Status");
+});
+
+
+
+// app.get("/d14", function (_req, _res) {
+//   // Collection of parameter.
+//   const d14date = new Date();
+//   const fd14date  = d14date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
+//   var code = _req.query.code;
+//   console.log(fd14date + " : " + code)
+
+//   const parameters = [{ name: "v_MCCode", value: code }];
+
 //   database.executeStoredProcedure(
-//     res,
+//     _res,
 //     kcvldbConfig,
 //     "RPT_D14_Machine_Running_Status",
 //     parameters
 //   );
-// }
-app.get("/d14", function (_req, _res) {
-  // Collection of parameter.
-  const d14date = new Date();
-  const fd14date  = d14date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
-  var code = _req.query.code;
-  console.log(fd14date + " : " + code)
+// });
 
-  const parameters = [{ name: "v_MCCode", value: code }];
+// app.get("/d15", function (_req, _res) {
+//   // Collection of parameter.
+//   const d15date = new Date();
+//   const fd15date  = d15date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
+//   var code = _req.query.code;
+//   console.log(fd15date +" : " + code)
 
-  database.executeStoredProcedure(
-    _res,
-    kcvldbConfig,
-    "RPT_D14_Machine_Running_Status",
-    parameters
-  );
-});
+//   const parameters = [{ name: "v_MCCode", value: code }];
 
-app.get("/d15", function (_req, _res) {
-  // Collection of parameter.
-  const d15date = new Date();
-  const fd15date  = d15date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
-  var code = _req.query.code;
-  console.log(fd15date +" : " + code)
+//   database.executeStoredProcedure(
+//     _res,
+//     kcvldbConfig,
+//     "RPT_D15_Machine_Running_Status",
+//     parameters
+//   );
+// });
 
-  const parameters = [{ name: "v_MCCode", value: code }];
+// app.get("/d16", function (_req, _res) {
+//   // Collection of parameter.
+//   const d16date = new Date();
+//   const fd16date  = d16date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
+//   var code = _req.query.code;
+//   console.log(fd16date + " : " + code)
 
-  database.executeStoredProcedure(
-    _res,
-    kcvldbConfig,
-    "RPT_D15_Machine_Running_Status",
-    parameters
-  );
-});
+//   const parameters = [{ name: "v_MCCode", value: code }];
 
-app.get("/d16", function (_req, _res) {
-  // Collection of parameter.
-  const d16date = new Date();
-  const fd16date  = d16date.toLocaleString('en-US',{weekday:'short',year:'numeric',month:'short',day:'numeric',hour:'numeric',minute:'numeric',second:'numeric',hour12: true})
-  var code = _req.query.code;
-  console.log(fd16date + " : " + code)
-
-  const parameters = [{ name: "v_MCCode", value: code }];
-
-  database.executeStoredProcedure(
-    _res,
-    kcvldbConfig,
-    "RPT_D16_Machine_Running_Status",
-    parameters
-  );
-});
+//   database.executeStoredProcedure(
+//     _res,
+//     kcvldbConfig,
+//     "RPT_D16_Machine_Running_Status",
+//     parameters
+//   );
+// });
